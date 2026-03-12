9ddd55676814eceafa199808b6d0b8f3e1a30366
Nando13 <nandoclb13@gmail.com>
2026-03-12 16:49:47 +0000

    Se completo ejercicio nodo.c

--- nodo/nodo.c
+++ nodo/nodo.c
@@ -1,15 +1,14 @@
 #include <stdlib.h>
 #include "nodo.h"
 
-Nodo* nodo_crear(int dato)
-{
-    Nodo *nuevo = (Nodo*)malloc(sizeof(Nodo));
-    if( nuevo != NULL){
-         nuevo->dato=dato;
-         nuevo->siguiente=NULL;
-    }
-    return nuevo;
+Nodo*nuevo= nodo_crear(int dato);
+Nodo *nuevo = (Nodo*)malloc(sizeof(Nodo));
+if( nuevo != NULL){
+     nuevo->dato=dato;
+     nuevo->siguiente=NULL;
 }
+return nuevo;
+
 void nodo_destruir(Nodo* nodo){
     if( nodo != NULL){
         free(nodo);
